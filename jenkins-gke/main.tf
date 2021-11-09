48a5baf8d9ed901d6663b74269f4ddf0dc9ba50e
danylovanin <danylo.vanin@modex.one>
2021-11-09 19:04:01 +0200

    updated values for jenkins

--- jenkins-gke/main.tf
+++ jenkins-gke/main.tf
@@ -198,10 +198,9 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
-  version    = "1.9.18"
   timeout    = 1200
 
-  values = [data.local_file.helm_chart_values.content]
+  values = ["${file("values.yaml")}"]
 
   depends_on = [
     kubernetes_secret.gh-secrets,
