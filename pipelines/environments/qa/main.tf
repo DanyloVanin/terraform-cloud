47e5afc9ac49aec372c3df475ec5e4babc7b8b60
danylovanin <danylo.vanin@modex.one>
2021-11-16 21:39:25 +0200

    updated all user invoke policy

--- pipelines/environments/qa/main.tf
+++ pipelines/environments/qa/main.tf
@@ -28,7 +28,7 @@ module "project-services" {
 /*****************************************
   Create a GCE VM Instance
  *****************************************/
-resource "google_cloud_run_service" "run_service" {
+resource "google_cloud_run_service" "default" {
   name     = "cloudrun-qa"
   location = var.subnet1_region
   project = var.project_id
@@ -49,9 +49,19 @@ resource "google_cloud_run_service" "run_service" {
 }
 
 # Allow unauthenticated users to invoke the service
-resource "google_cloud_run_service_iam_member" "run_all_users" {
-  service  = google_cloud_run_service.run_service.name
-  location = google_cloud_run_service.run_service.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
