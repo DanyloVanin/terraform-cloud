5e9f8385f8d096149fd22a7fbb66f6ebfbbf348c
danylovanin <danylo.vanin@modex.one>
2021-11-16 22:22:54 +0200

    updated policy binding

--- pipelines/environments/qa/main.tf
+++ pipelines/environments/qa/main.tf
@@ -49,19 +49,12 @@ resource "google_cloud_run_service" "default" {
 }
 
 # Allow unauthenticated users to invoke the service
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.default.location
-  project     = google_cloud_run_service.default.project
-  service     = google_cloud_run_service.default.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
+resource "google_cloud_run_service_iam_binding" "binding" {
+  location = google_cloud_run_service.default.location
+  project = google_cloud_run_service.default.project
+  service = google_cloud_run_service.default.name
+  role = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
 }
